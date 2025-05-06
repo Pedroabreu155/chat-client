bcf8e75e5a3356be5437ad34787104a074605683
Pedroabreu155 <pedroliveirabreu@gmail.com>
2025-05-06 19:28:43 -0300

    add logs

--- client.ts
+++ client.ts
@@ -3,6 +3,15 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const message_data = {
+  username: process.env.USERNAME,
+  room: process.env.TRAVEL_ID,
+  userId: process.env.USER_ID,
+  travelId: process.env.TRAVEL_ID,
+  message: '',
+  chatId: 0,
+};
+
 const socket = io(`${process.env.CHAT_URL}`, {
   extraHeaders: {
     authorization: `Bearer ${process.env.TOKEN}`,
@@ -20,6 +29,19 @@ socket.emit('select_travel_room', {
   travelId: process.env.TRAVEL_ID,
 });
 
+socket.on('connect_error', (error) => {
+  console.log('Erro de conexão:', error);
+});
+
 socket.on('old_messages', (data) => {
   console.log(data);
+  message_data.chatId = data.chatId;
+  message_data.message = 'Teste de mensagem';
+
+  socket.emit('message', message_data);
+});
+
+socket.on('message', (data) => {
+  console.log(data);
+  console.log('mensagem foi inserida');
 });
